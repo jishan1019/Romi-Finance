34b5f7e22969225ca49a3918414c40c0791591fb
jishan1019 <imranislamjishan200@gmail.com>
2023-10-28 14:12:55 +0600

    navbar responsive make

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./Pages/Share/Footer/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="bg-[#191919] text-white max-w-[1600px] mx-auto">
       <Header />
       <div className="min-h-[calc(100vh-100px)]">
         <Outlet />
